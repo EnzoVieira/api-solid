39d0e45b405acbf00d88eb053edda003b083fb27
Enzo Vieira <enzovieira16@hotmail.com>
2023-04-08 18:36:50 +0100

    feat(CheckIn): Gyms related tests, like distance and exists gym

--- src/use-cases/check-in.ts
+++ src/use-cases/check-in.ts
@@ -1,10 +1,15 @@
 import type { CheckIn } from "@prisma/client"
 
 import { CheckInsRepository } from "@/repositories/check-ins-repository"
+import { GymsRepository } from "@/repositories/gyms-repository"
+import { ResourceNotFoundError } from "./errors/resource-not-found-error"
+import { getDistanceBetweenCoordinates } from "@/utils/get-distance-between-coordinates"
 
 interface CheckInUseCaseRequest {
   userId: string
   gymId: string
+  userLatitude: number
+  userLongitude: number
 }
 
 interface CheckInUseCaseResponse {
@@ -12,12 +17,43 @@ interface CheckInUseCaseResponse {
 }
 
 export class CheckInUseCase {
-  constructor(private checkInsRepository: CheckInsRepository) {}
+  constructor(
+    private checkInsRepository: CheckInsRepository,
+    private gymsRepository: GymsRepository
+  ) {}
 
   async execute({
     userId,
     gymId,
+    userLatitude,
+    userLongitude,
   }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
+    const gym = await this.gymsRepository.findById(gymId)
+
+    if (!gym) {
+      throw new ResourceNotFoundError()
+    }
+
+    const distance = getDistanceBetweenCoordinates(
+      { latitude: userLatitude, longitude: userLongitude },
+      { latitude: gym.latitude.toNumber(), longitude: gym.longitude.toNumber() }
+    )
+
+    const MAX_DISTANCE_IN_KM = 0.1 // Distance in Km (100m)
+
+    if (distance > MAX_DISTANCE_IN_KM) {
+      throw new Error()
+    }
+
+    const checkInOnSameDate = await this.checkInsRepository.findByUserIdOnDate(
+      userId,
+      new Date()
+    )
+
+    if (checkInOnSameDate) {
+      throw new Error()
+    }
+
     const checkIn = await this.checkInsRepository.create({
       user_id: userId,
       gym_id: gymId,
